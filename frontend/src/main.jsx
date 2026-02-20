d8b4cf256c37d3cda656f5e58839c6fe681914a6
Rohan Igave <igaverohan54@gmail.com>
2026-02-20 16:55:44 +0530

    Connected frontend to production backend URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:5001";
+export const server = import.meta.env.VITE_API_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
